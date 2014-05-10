1bb696ed73362acc93d52589358c2481b4291bac
zunath <coolty3001@yahoo.com>
2014-05-10 18:07:27 -0500

    Tileset class

---
+++ Ceriyo.Data/GameObjects/Tileset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ceriyo.Data.Enumerations;
+
+namespace Ceriyo.Data.GameObjects
+{
+    public class Tileset : IGameObject
+    {
+        public string Name { get; set; }
+        public string Tag { get; set; }
+        public string Resref { get; set; }
+        public string Description { get; set; }
+        public string Comments { get; set; }
+        public string WorkingDirectory { get { return WorkingPaths.TilesetsDirectory; } }
+        public IList<LocalVariable> LocalVariables { get; set; }
+        public IDictionary<ScriptEventTypeEnum, string> Scripts
+        {
+            get { throw new NotSupportedException(); }
+            set { throw new NotSupportedException(); }
+        }
+    }
+}
