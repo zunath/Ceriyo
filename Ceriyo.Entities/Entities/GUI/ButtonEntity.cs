125b3ab8af838ed37635aa579fc53853668661ee
zunath <coolty3001@yahoo.com>
2014-06-25 10:34:20 -0500

    Button entity stubbing

---
+++ Ceriyo.Entities/Entities/GUI/ButtonEntity.cs
@@ -0,0 +1,33 @@
+﻿using Ceriyo.Data.ResourceObjects;
+using Ceriyo.Library.Processing;
+using FlatRedBall;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ceriyo.Entities.Entities.GUI
+{
+    public class ButtonEntity : GraphicEntity
+    {
+        public ButtonEntity(GameResource graphic)
+            : base("ButtonEntity", graphic)
+        {
+        }
+
+        protected override void CustomInitialize()
+        {
+            
+        }
+
+        protected override void CustomActivity()
+        {
+            
+        }
+
+        protected override void CustomDestroy()
+        {
+            
+        }
+    }
+}
---
+++ Ceriyo.Entities/Entities/GraphicEntity.cs
@@ -0,0 +1,28 @@
+﻿using Ceriyo.Data.ResourceObjects;
+using Ceriyo.Library.Processing;
+using FlatRedBall;
+using FlatRedBall.Graphics.Animation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ceriyo.Entities.Entities
+{
+    public abstract class GraphicEntity : BaseEntity
+    {
+        private GameResourceProcessor Processor { get; set; }
+        protected GameResource GraphicResource { get; set; }
+        protected Sprite EntitySprite { get; set; }
+        
+        public GraphicEntity(string contentManagerName, GameResource graphic)
+            : base(contentManagerName)
+        {
+            this.GraphicResource = graphic;
+            this.EntitySprite = new Sprite();
+            this.EntitySprite.Visible = true;
+            
+        }
+
+    }
+}
