13cddbc51cdfe2b336186c7d38d647b1b429abae
zunath <coolty3001@yahoo.com>
2014-06-29 00:29:59 -0500

    Added direction type enum

---
+++ Ceriyo.Data/Enumerations/DirectionTypeEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ceriyo.Data.Enumerations
+{
+    public enum DirectionTypeEnum
+    {
+        Unknown = 0,
+        North = 1,
+        South = 2,
+        East = 3,
+        West = 4,
+        Southeast = 5,
+        Southwest = 6,
+        Northeast = 7,
+        Northwest = 8
+    }
+}
