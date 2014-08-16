2392e99016d665b5119edd1ba7333961cdb12a6d
zunath <coolty3001@yahoo.com>
2014-08-15 20:59:13 -0500

    Move SampleScene logic to GUIDrawableBatch

--- Ceriyo.Entities/DrawableBatches/GUIDrawableBatch.cs
+++ Ceriyo.Entities/DrawableBatches/GUIDrawableBatch.cs
@@ -6,29 +6,29 @@ using FlatRedBall;
 using FlatRedBall.Graphics;
 using Ceriyo.Library.SquidGUI;
 using Squid;
+using SampleControls;
 
 namespace Ceriyo.Entities.DrawableBatches
 {
     public class GUIDrawableBatch : PositionedObject, IDrawableBatch
     {
         private SquidInputManager _inputManager;
-        private SampleScene _scene;
+        private Desktop _desktop;
 
         public GUIDrawableBatch()
         {
+            _desktop = new SampleDesktop { Name = "desk" };
+            _desktop.ShowCursor = true;
+
             GuiHost.Renderer = new SquidRendererXNA();
             _inputManager = new SquidInputManager();
             FlatRedBallServices.Game.Components.Add(_inputManager);
-
-            _scene = new SampleScene();
-            FlatRedBallServices.Game.Components.Add(_scene);
 
             SpriteManager.AddDrawableBatch(this);
         }
 
         public void Destroy()
         {
-            FlatRedBallServices.Game.Components.Remove(_scene);
             FlatRedBallServices.Game.Components.Remove(_inputManager);
 
             GuiHost.Renderer = null;
@@ -39,6 +39,11 @@ namespace Ceriyo.Entities.DrawableBatches
         public void Draw(Camera camera)
         {
             GuiHost.TimeElapsed = (float)TimeManager.LastUpdateGameTime.ElapsedGameTime.TotalMilliseconds;
+
+            _desktop.Size = new Squid.Point(FlatRedBallServices.Game.GraphicsDevice.Viewport.Width, 
+                FlatRedBallServices.Game.GraphicsDevice.Viewport.Height);
+            _desktop.Update();
+            _desktop.Draw();
 
         }
 
