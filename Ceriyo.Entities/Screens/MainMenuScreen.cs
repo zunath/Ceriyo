004de790a99cff74d5fc28b372ae9b823632e68e
zunath <coolty3001@yahoo.com>
2014-08-15 21:15:15 -0500

    Remove old button and check box entities

--- Ceriyo.Entities/Screens/MainMenuScreen.cs
+++ Ceriyo.Entities/Screens/MainMenuScreen.cs
@@ -10,38 +10,22 @@ namespace Ceriyo.Entities.Screens
 {
     public class MainMenuScreen : BaseScreen
     {
-        private ButtonEntity ConnectButton { get; set; }
-        private ButtonEntity ExitButton { get; set; }
-        private Layer GUILayer { get; set; }
 
         public MainMenuScreen()
             : base("MainMenuScreen")
         {
-            GUILayer = new Layer();
-            ConnectButton = new ButtonEntity("Connect");
-            ExitButton = new ButtonEntity("Exit");
         }
 
         protected override void CustomInitialize()
         {
-            SpriteManager.Camera.AddLayer(GUILayer);
-            ConnectButton.InitializeEntity(true);
-            ExitButton.InitializeEntity(true);
-
-            ConnectButton.Y += 50;
         }
 
         protected override void CustomActivity(bool firstTimeCalled)
         {
-            ConnectButton.Activity();
-            ExitButton.Activity();
         }
 
         protected override void CustomDestroy()
         {
-            SpriteManager.Camera.RemoveLayer(GUILayer);
-            ConnectButton.Destroy();
-            ExitButton.Destroy();
         }
     }
 }
