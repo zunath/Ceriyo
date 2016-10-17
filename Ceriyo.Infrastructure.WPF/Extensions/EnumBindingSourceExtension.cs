63a3c7f29f8a5b5aa12a8c72078e92cd64c727d8
zunath <coolty3001@yahoo.com>
2016-10-16 22:36:51 -0500

    Minor cleanup

--- Ceriyo.Infrastructure.WPF/Extensions/EnumBindingSourceExtension.cs
+++ Ceriyo.Infrastructure.WPF/Extensions/EnumBindingSourceExtension.cs
@@ -8,21 +8,19 @@ namespace Ceriyo.Infrastructure.WPF.Extensions
         private Type _enumType;
         public Type EnumType
         {
-            get { return this._enumType; }
+            get { return _enumType; }
             set
             {
-                if (value != this._enumType)
+                if (value == _enumType) return;
+                if (null != value)
                 {
-                    if (null != value)
-                    {
-                        Type enumType = Nullable.GetUnderlyingType(value) ?? value;
+                    Type enumType = Nullable.GetUnderlyingType(value) ?? value;
 
-                        if (!enumType.IsEnum)
-                            throw new ArgumentException("Type must be for an Enum.");
-                    }
-
-                    this._enumType = value;
+                    if (!enumType.IsEnum)
+                        throw new ArgumentException("Type must be for an Enum.");
                 }
+
+                _enumType = value;
             }
         }
 
@@ -30,18 +28,18 @@ namespace Ceriyo.Infrastructure.WPF.Extensions
 
         public EnumBindingSourceExtension(Type enumType)
         {
-            this.EnumType = enumType;
+            EnumType = enumType;
         }
 
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            if (null == this._enumType)
+            if (null == _enumType)
                 throw new InvalidOperationException("The EnumType must be specified.");
 
-            Type actualEnumType = Nullable.GetUnderlyingType(this._enumType) ?? this._enumType;
+            Type actualEnumType = Nullable.GetUnderlyingType(_enumType) ?? _enumType;
             Array enumValues = Enum.GetValues(actualEnumType);
 
-            if (actualEnumType == this._enumType)
+            if (actualEnumType == _enumType)
                 return enumValues;
 
             Array tempArray = Array.CreateInstance(actualEnumType, enumValues.Length + 1);
